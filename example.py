337e972934a0cbd8fc5f3fdeaaff3ee0c7e8d3da
Tsuyoshi IMOTO <apto.as@gmail.com>
2023-05-06 22:33:04 +0900

    stream example add.

--- example.py
+++ example.py
@@ -1,23 +1,27 @@
-# pip install scipy numpy
-import numpy as np
-from scipy.io import wavfile
+from io import BytesIO
 
+import numpy as np
+import soundfile as sf
+import speech_recognition as sr
 import pywhisper
 
-# Load whisper
-assert pywhisper.init(model_path="../../models/ggml-base.en.bin"), (
-    "Failed to initialize pywhisper. Check the logs above for more details."
-)
+if __name__ == "__main__":
+    # Load whisper
+    assert pywhisper.init(model_path="../whisper.cpp/models/ggml-medium.bin"), (
+        "Failed to initialize pywhisper. Check the logs above for more details."
+    )
+    recognizer = sr.Recognizer()
+    while True:
+        with sr.Microphone(sample_rate=16_000) as source:
+            print("なにか話してください")
+            audio = recognizer.listen(source)
 
-# Load audio
-sample_rate, data = wavfile.read('../../samples/jfk.wav')
-assert sample_rate == 16000, "Only 16kHz audio is supported"
+        print("音声処理中 ...")
+        # 「音声データをWhisperの入力形式に変換」参照
+        wav_bytes = audio.get_wav_data()
+        wav_stream = BytesIO(wav_bytes)
+        audio_array, sampling_rate = sf.read(wav_stream)
+        audio_fp32 = audio_array.astype(np.float32)
 
-# Normalize the audio data to the range [-1, 1]
-if not np.issubdtype(data.dtype, np.floating):
-    data = data.astype(np.float32) / np.iinfo(data.dtype).max
-
-transcription = pywhisper.transcribe(data)
-print(transcription)
-# Should print something like:
-# [(0.0, 11.0, ' And so my fellow Americans, ask not what your country can do for you, ask what you can do for your country.')]
+        transcription = pywhisper.transcribe(audio_fp32, "ja")
+        print(transcription[0][2])
